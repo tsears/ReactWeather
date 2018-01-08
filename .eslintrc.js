fcc39ef29a4c8a4e60a2bf94e4704c5f58704634
Tom Sears <tom@tsears.net>
2018-01-07 18:30:05 -0800

    Lint

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "jasmine": true,
+        "protractor": true,
+    },
+    plugins: [ "react", ],
+    "extends": "standard",
+    globals: {
+        CKEDITOR: true,
+        // To get to the wiris editor, one uses com.wiris.JsEditor.
+        com: true,
+    },
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        'valid-jsdoc': 1,
+        'comma-dangle': [ 'error', {
+            arrays: 'always-multiline',
+            objects: 'always-multiline',
+            imports: 'always-multiline',
+            exports: 'always-multiline',
+            functions: 'ignore',
+        }],
+        indent: ['error', 2],
+        'max-len': [1, 120, { ignoreUrls: true, ignoreStrings: true }],
+        'one-var': 1,
+        'standard/computed-property-even-spacing': 1,
+        'no-useless-escape': 1,
+        'new-cap': [2, {
+            capIsNewExceptions: [
+                'CKEDITOR',
+            ],
+            capIsNewExceptionPattern: '^CKEDITOR\.|CKEDITOR\.dom\.|jQuery\.Event|angular\.element\.Event',
+            newIsCapExceptionPattern: '^CKEDITOR\.|CKEDITOR\.dom\.|jQuery\.Event',
+        }],
+        'no-throw-literal': 1,
+    },
+};
